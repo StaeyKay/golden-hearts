bf2692abc9d1e156b16243bbedb34c4c02d973df
StaeyKay <ekwashie001@st.ug.edu.gh>
2024-09-11 12:18:11 +0000

    Modified padding to suit mobile phone responsiveness

--- src/components/about.jsx
+++ src/components/about.jsx
@@ -1,24 +1,64 @@
-import React from 'react'
+import React from "react";
 
 const About = () => {
-    return (
-        <div className='space-y-8 p-[100px] text-justify'>
+  return (
+    <div className="p-[30px] space-y-8 md:p-[100px] text-justify">
+      <div className="flex flex-col">
+        <span className="text-[30px] text-[#916c06] font-bold flex flex-col">
+          About Us
+        </span>
+        <span>
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit. Curabitur
+          facilisis fringilla quam ac rutrum. Vivamus a dui ut ipsum gravida
+          varius. Mauris tincidunt suscipit purus nec blandit. Nulla varius
+          sollicitudin metus, at ultricies erat scelerisque vel. Lorem ipsum
+          dolor sit amet, consectetur adipiscing elit. Donec eleifend quam nunc,
+          sed sodales metus suscipit ut. Etiam ut nulla ut mi accumsan faucibus
+          at fermentum turpis. Nulla aliquam posuere ultricies. Ut vitae enim in
+          ligula pharetra bibendum quis quis nulla. Quisque aliquam eget lorem
+          ac convallis. Nam sem erat, varius vestibulum ligula sit amet,
+          convallis fringilla justo. Phasellus feugiat vehicula nunc quis
+          volutpat.
+        </span>
+      </div>
+      <div className="flex flex-col">
+        <span className="text-[30px] text-[#916c06] font-bold flex flex-col">
+          Our Mission
+        </span>
+        <span>
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit. Curabitur
+          facilisis fringilla quam ac rutrum. Vivamus a dui ut ipsum gravida
+          varius. Mauris tincidunt suscipit purus nec blandit. Nulla varius
+          sollicitudin metus, at ultricies erat scelerisque vel. Lorem ipsum
+          dolor sit amet, consectetur adipiscing elit. Donec eleifend quam nunc,
+          sed sodales metus suscipit ut. Etiam ut nulla ut mi accumsan faucibus
+          at fermentum turpis. Nulla aliquam posuere ultricies. Ut vitae enim in
+          ligula pharetra bibendum quis quis nulla. Quisque aliquam eget lorem
+          ac convallis. Nam sem erat, varius vestibulum ligula sit amet,
+          convallis fringilla justo. Phasellus feugiat vehicula nunc quis
+          volutpat.
+        </span>
+      </div>
+      <div className="flex flex-col">
+        <span className="text-[30px] text-[#916c06] font-bold flex flex-col">
+          Our Vision
+        </span>
+        <span>
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit. Curabitur
+          facilisis fringilla quam ac rutrum. Vivamus a dui ut ipsum gravida
+          varius. Mauris tincidunt suscipit purus nec blandit. Nulla varius
+          sollicitudin metus, at ultricies erat scelerisque vel. Lorem ipsum
+          dolor sit amet, consectetur adipiscing elit. Donec eleifend quam nunc,
+          sed sodales metus suscipit ut. Etiam ut nulla ut mi accumsan faucibus
+          at fermentum turpis. Nulla aliquam posuere ultricies. Ut vitae enim in
+          ligula pharetra bibendum quis quis nulla. Quisque aliquam eget lorem
+          ac convallis. Nam sem erat, varius vestibulum ligula sit amet,
+          convallis fringilla justo. Phasellus feugiat vehicula nunc quis
+          volutpat.
+        </span>
+      </div>
+    </div>
+  );
+};
 
-            <div className='flex flex-col'>
-                <span className='text-[30px] text-[#916c06] font-bold flex flex-col'>About Us</span>
-                <span>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Curabitur facilisis fringilla quam ac rutrum. Vivamus a dui ut ipsum gravida varius. Mauris tincidunt suscipit purus nec blandit. Nulla varius sollicitudin metus, at ultricies erat scelerisque vel. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec eleifend quam nunc, sed sodales metus suscipit ut. Etiam ut nulla ut mi accumsan faucibus at fermentum turpis. Nulla aliquam posuere ultricies. Ut vitae enim in ligula pharetra bibendum quis quis nulla. Quisque aliquam eget lorem ac convallis. Nam sem erat, varius vestibulum ligula sit amet, convallis fringilla justo. Phasellus feugiat vehicula nunc quis volutpat.</span>
-            </div>
-            <div className='flex flex-col'>
-                <span className='text-[30px] text-[#916c06] font-bold flex flex-col'>Our Mission</span>
-                <span>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Curabitur facilisis fringilla quam ac rutrum. Vivamus a dui ut ipsum gravida varius. Mauris tincidunt suscipit purus nec blandit. Nulla varius sollicitudin metus, at ultricies erat scelerisque vel. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec eleifend quam nunc, sed sodales metus suscipit ut. Etiam ut nulla ut mi accumsan faucibus at fermentum turpis. Nulla aliquam posuere ultricies. Ut vitae enim in ligula pharetra bibendum quis quis nulla. Quisque aliquam eget lorem ac convallis. Nam sem erat, varius vestibulum ligula sit amet, convallis fringilla justo. Phasellus feugiat vehicula nunc quis volutpat.</span>
-            </div>
-            <div className='flex flex-col'>
-                <span className='text-[30px] text-[#916c06] font-bold flex flex-col'>Our Vision</span>
-                <span>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Curabitur facilisis fringilla quam ac rutrum. Vivamus a dui ut ipsum gravida varius. Mauris tincidunt suscipit purus nec blandit. Nulla varius sollicitudin metus, at ultricies erat scelerisque vel. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec eleifend quam nunc, sed sodales metus suscipit ut. Etiam ut nulla ut mi accumsan faucibus at fermentum turpis. Nulla aliquam posuere ultricies. Ut vitae enim in ligula pharetra bibendum quis quis nulla. Quisque aliquam eget lorem ac convallis. Nam sem erat, varius vestibulum ligula sit amet, convallis fringilla justo. Phasellus feugiat vehicula nunc quis volutpat.</span>
-            </div>
-
-        </div>
-    )
-}
-
-export default About
+export default About;
